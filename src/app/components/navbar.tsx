13aaddaefcdd8f7346600f1b347ed57b58ae68fa
kaushalrijal <kaushalrijal091@gmail.com>
2024-01-05 17:52:38 +0545

    minor tweaks to the navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   const pathname = usePathname();
   return (
     <div className="sticky top-0 w-full z-10 rounded-sm">
-      <div className="flex items-center justify-between w-full px-2.5 bg-white backdrop-blur-sm z-10 md:justify-center md:gap-6 py-2 md:py-4 text-lg sm:text-base">
+      <div className="flex items-center justify-between w-full sm:px-2.5 bg-white backdrop-blur-sm z-10 md:justify-center md:gap-6 py-2 md:py-4 text-base">
         {navitems.map((item) => {
           return (
             <Link href={item.href} key={item.id}>
