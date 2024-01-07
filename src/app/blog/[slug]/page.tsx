641cccdd0a426e6212f746e15dfdad391210fb11
kaushalrijal <kaushalrijal091@gmail.com>
2024-01-07 12:34:56 +0545

    slug added

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Blogs = ({ posts }) => {
+  return <div>{posts?.slug}</div>;
+};
--- src/app/blog/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Blogs = () => {
-  return <div>Blogs go here</div>;
-};
-
-export default Blogs;
