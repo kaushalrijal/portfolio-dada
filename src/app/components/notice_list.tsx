f4b4fd57f34c159c862c07f002c1dcb3d741feac
kaushalrijal <kaushalrijal091@gmail.com>
2024-01-10 13:07:17 +0545

    link updated for notices section

--- src/app/components/notice_list.tsx
+++ src/app/components/notice_list.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import NoticeItem from "./notice_item";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 const Notices = () => {
   return (
     <div className="mb-4">
       <span className="text-secondary text-3xl font-semibold gap-1 flex justify-between items-center">
         Notices
-        <div className="flex items-end text-xs font-normal hover:text-primary cursor-pointer hover:underline">
-          View all
-          <ArrowRight className="w-3 h-auto pb-0.5" />
-        </div>
+        <Link
+          href="https://psc.gov.np/category/recruitment-notices.html"
+          target="none"
+        >
+          <div className="flex items-end text-xs font-normal hover:text-primary cursor-pointer hover:underline">
+            View all
+            <ArrowRight className="w-3 h-auto pb-0.5" />
+          </div>
+        </Link>
       </span>
       <NoticeItem
         title="बागमती || स्वास्थ्य सेवा || सहायक पाँचौ || प.हे.न. || पब्लिक हेल्थ नर्स (Public Health Nurse)"
