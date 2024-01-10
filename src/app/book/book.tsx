85ab92ad797665e20433f7e8651124abdda74dee
kaushalrijal <kaushalrijal091@gmail.com>
2024-01-10 19:57:35 +0545

    more minor tweaks and now books page is fully working

--- src/app/book/book.tsx
+++ src/app/book/book.tsx
@@ -1,10 +1,46 @@
 import React from "react";
 
-const Book = (props) => {
+const Book = (props: {
+  image: string | undefined;
+  title:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | Iterable<React.ReactNode>
+    | React.ReactPortal
+    | React.PromiseLikeOfReactNode
+    | null
+    | undefined;
+  price:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | Iterable<React.ReactNode>
+    | React.ReactPortal
+    | React.PromiseLikeOfReactNode
+    | null
+    | undefined;
+  subdetails:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | Iterable<React.ReactNode>
+    | React.ReactPortal
+    | React.PromiseLikeOfReactNode
+    | null
+    | undefined;
+}) => {
   return (
     <div className="w-full flex flex-row gap-3">
-      <img className="w-24 sm:w-32 h-auto" alt="book image" src={props.image} />
-      <div className="flex flex-col gap-1">
+      <img
+        className="w-1/5 flex flex-nowrap"
+        alt="book image"
+        src={props.image}
+      />
+      <div className="flex flex-col gap-1 w-4/5">
         <span className="text-2xl sm:text-3xl text-primary font-semibold">
           {props.title}
         </span>
--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -1,14 +1,87 @@
 import React from "react";
 import Book from "./book";
+import { client, urlFor } from "../../../client";
 
-const Books = () => {
+const getData = async () => {
+  const query = `
+  *[_type == "book"]| order(_createdAt desc) {
+    title,
+      price,
+      bookImage,
+      "slug": slug.current,
+      subdetail
+  }
+  `;
+  const data = await client.fetch(query);
+  return data;
+};
+
+const Books = async () => {
+  const books = await getData();
+
   return (
     <div className="w-full">
       <span className="text-3xl font-semibold italic text-primary items-start w-full my-4">
         Books
       </span>
-      <div className="my-4">
-        <Book />
+      <div className="my-4 gap-2 flex flex-col">
+        {books.map(
+          (
+            book: {
+              bookImage: any;
+              title:
+                | string
+                | number
+                | boolean
+                | React.ReactPortal
+                | React.PromiseLikeOfReactNode
+                | React.ReactElement<
+                    any,
+                    string | React.JSXElementConstructor<any>
+                  >
+                | Iterable<React.ReactNode>
+                | null
+                | undefined;
+              price:
+                | string
+                | number
+                | boolean
+                | React.ReactPortal
+                | React.PromiseLikeOfReactNode
+                | React.ReactElement<
+                    any,
+                    string | React.JSXElementConstructor<any>
+                  >
+                | Iterable<React.ReactNode>
+                | null
+                | undefined;
+              subdetail:
+                | string
+                | number
+                | boolean
+                | React.ReactPortal
+                | React.PromiseLikeOfReactNode
+                | React.ReactElement<
+                    any,
+                    string | React.JSXElementConstructor<any>
+                  >
+                | Iterable<React.ReactNode>
+                | null
+                | undefined;
+            },
+            id: React.Key | null | undefined
+          ) => {
+            return (
+              <Book
+                image={urlFor(book.bookImage).url()}
+                title={book.title}
+                price={book.price}
+                subdetails={book.subdetail}
+                key={id}
+              />
+            );
+          }
+        )}
       </div>
     </div>
   );
