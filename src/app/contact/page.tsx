b597e9c64b35b105c3d8bf3d036cc3eb0dfefe17
kaushalrijal <kaushalrijal091@gmail.com>
2024-01-11 18:36:22 +0545

    form added

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,15 +2,26 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="w-full">
-      <span className="w-full my-4 text-2xl text-primary italic font-semibold">
+    <div className="w-full my-20">
+      <span className="w-full my-8 text-2xl text-primary italic font-semibold">
         सम्पर्क
       </span>
-      <div className=" my-4">
-        <form className="w-full flex flex-col  gap-3">
-          <input type="text" placeholder="नाम" />
-          <input type="email" placeholder="Email" />
-          <input type="textarea" placeholder="Your Message" />
+      <div className=" my-8">
+        <form className="w-full flex flex-col gap-3">
+          <input
+            className="p-1 border-black focus:border-primary focus:outline-none border-[2px] text-black placeholder:text-black rounded-md"
+            type="text"
+            placeholder="नाम"
+          />
+          <input
+            className="p-1 border-black focus:border-primary focus:outline-none border-[2px] text-black placeholder:text-black rounded-md"
+            type="email"
+            placeholder="Email"
+          />
+          <textarea
+            className="p-1 border-black focus:border-primary focus:outline-none border-[2px] text-black placeholder:text-black rounded-md"
+            placeholder="Your Message"
+          />
           <input type="submit" />
         </form>
       </div>
