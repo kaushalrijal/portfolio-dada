f746b991e514bb45884387acd889a25f9c23fa66
kaushalrijal <kaushalrijal091@gmail.com>
2024-01-11 18:19:44 +0545

    form added

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,7 +7,7 @@ const Contact = () => {
         सम्पर्क
       </span>
       <div className=" my-4">
-        <form className="w-full flex flex-col ">
+        <form className="w-full flex flex-col  gap-3">
           <input type="text" placeholder="नाम" />
           <input type="email" placeholder="Email" />
           <input type="textarea" placeholder="Your Message" />
