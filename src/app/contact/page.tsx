0a79fcf93760be290d4f01b04c737453e342c4bd
kaushalrijal <kaushalrijal091@gmail.com>
2024-01-11 18:18:42 +0545

    header of contact form changed

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="w-full">
+      <span className="w-full my-4 text-2xl text-primary italic font-semibold">
+        सम्पर्क
+      </span>
+      <div className=" my-4">
+        <form className="w-full flex flex-col ">
+          <input type="text" placeholder="नाम" />
+          <input type="email" placeholder="Email" />
+          <input type="textarea" placeholder="Your Message" />
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
