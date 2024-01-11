a8bdc3f2b67a3a06a15e1f5ce2374fcff6d7985f
kaushalrijal <kaushalrijal091@gmail.com>
2024-01-11 18:39:32 +0545

    form ui updated

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,20 +9,26 @@ const Contact = () => {
       <div className=" my-8">
         <form className="w-full flex flex-col gap-3">
           <input
-            className="p-1 border-black focus:border-primary focus:outline-none border-[2px] text-black placeholder:text-black rounded-md"
+            className="p-1 border-black focus:border-primary focus:border-2 focus:outline-none border-[1px] text-black placeholder:text-black rounded-md"
             type="text"
             placeholder="नाम"
+            required
           />
           <input
-            className="p-1 border-black focus:border-primary focus:outline-none border-[2px] text-black placeholder:text-black rounded-md"
+            className="p-1 border-black focus:border-primary focus:border-2 focus:outline-none border-[1px] text-black placeholder:text-black rounded-md"
             type="email"
             placeholder="Email"
+            required
           />
           <textarea
-            className="p-1 border-black focus:border-primary focus:outline-none border-[2px] text-black placeholder:text-black rounded-md"
+            className="p-1 border-black focus:border-primary focus:border-2 focus:outline-none border-[1px] text-black placeholder:text-black rounded-md"
             placeholder="Your Message"
+            required
           />
-          <input type="submit" />
+          <input
+            type="submit"
+            className="w-full bg-primary p-2 rounded-md text-white cursor-pointer"
+          />
         </form>
       </div>
     </div>
